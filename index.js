2a4909d84eb5d298c679b4e07c3fe51c1acf3146
Veronica Miller <vmcmiller@gmail.com>
2019-03-24 17:53:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,15 +19,18 @@ class Driver {
   };
 
   passengers() {
-    let driverPassengers = [];
-    for (const trip of this.trips()) {
-      let tripPassenger = store.passengers.find(function(passenger) {
-        return passenger.id === trip.passengerId;
-        }.bind(this)
-      );
-      driverPassengers.push(tripPassenger);
-    };
-    return driverPassengers;
+    //let driverPassengers = [];
+    //for (const trip of this.trips()) {
+      //let tripPassenger = store.passengers.find(function(passenger) {
+        //return passenger.id === trip.passengerId;
+        //}.bind(this)
+      //);
+      //driverPassengers.push(tripPassenger);
+    //};
+    //return driverPassengers;
+    return this.trips.map(function(trip) {
+      return trip.driver();
+    });
   };
 
 };
