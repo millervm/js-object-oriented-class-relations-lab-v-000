b1572c89f8910331d1f252a7f528d884198a3efe
Veronica Miller <vmcmiller@gmail.com>
2019-03-24 17:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,15 +50,18 @@ class Passenger {
   };
 
   drivers() {
-    let passengerDrivers = [];
-    for (const trip of this.trips()) {
-      let tripDriver = store.drivers.find(function(driver) {
-        return driver.id === trip.driverId;
-        }.bind(this)
-      );
-      passengerDrivers.push(tripDriver);
-    };
-    return passengerDrivers;
+    //let passengerDrivers = [];
+    //for (const trip of this.trips()) {
+      //let tripDriver = store.drivers.find(function(driver) {
+      //  return driver.id === trip.driverId;
+        //}.bind(this)
+      //);
+      //passengerDrivers.push(tripDriver);
+    //};
+    //return passengerDrivers;
+    return this.trips().map(function(trip) {
+      return trip.driver();
+    });
   };
 
 };
